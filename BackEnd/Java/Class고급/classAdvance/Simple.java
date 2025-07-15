7f1667c4b057e1c350d89fa4b5870b77a3b30d0f
Jong Cheon Park <simpleego@gmail.com>
2025-07-15 14:34:01 +0900

    Add files via upload

---
+++ BackEnd/Java/Class고급/classAdvance/Simple.java
@@ -0,0 +1,31 @@
+package classAdvance;
+
+public class Simple {
+    private int age;
+    int num=20;
+    public String address;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ BackEnd/Java/Class고급/other/Other.java
@@ -0,0 +1,11 @@
+package other;
+
+import classAdvance.Simple;
+
+public class Other {
+    public static void main(String[] args) {
+        Simple simple = new Simple();
+        simple.address = "대전";
+        //simple
+    }
+}
