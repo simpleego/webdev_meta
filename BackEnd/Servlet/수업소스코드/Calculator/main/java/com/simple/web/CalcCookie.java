ca2af3f97d94ac74c8e7bf617273aef8f0f9e74e
Jong Cheon Park <simpleego@gmail.com>
2025-08-12 08:34:12 +0900

    Add files via upload

--- BackEnd/Servlet/수업소스코드/Calculator/main/java/com/simple/web/CalcCookie.java
+++ BackEnd/Servlet/수업소스코드/Calculator/main/java/com/simple/web/CalcCookie.java
@@ -3,17 +3,25 @@ package com.simple.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/calc2")
-public class Calc2 extends HttpServlet{
+@WebServlet("/calc_cookie")
+public class CalcCookie extends HttpServlet{
 	
+	static int count=0;
+	String  name;	
+	
+	public CalcCookie() {
+		name="홍길동";
+		System.out.println("생성자 호출됩니다..");
+	}
+
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
@@ -21,7 +29,15 @@ public class Calc2 extends HttpServlet{
 		//ServletContext application =  req.getServletContext();
 		
 		// Session 저장소 준비
-		HttpSession session = req.getSession();
+		// HttpSession session = req.getSession();
+		
+		//CalcCookie.count++;
+		count++;
+		
+		System.out.println(name);
+		
+		// Cookie 저장소 준비
+		Cookie[] cookies =  req.getCookies();
 		
 		res.setCharacterEncoding("UTF-8");
 		res.setContentType("text/html; charset=UTF-8");
@@ -46,11 +62,28 @@ public class Calc2 extends HttpServlet{
 		if(operator.equals("=")) {
 			// 두개의 정수를 저장소에서 꺼내서 계산
 			//int x = (int) application.getAttribute("value");
-			int x = (int) session.getAttribute("value");
+			// int x = (int) session.getAttribute("value");
+			int x=0;
+			for (Cookie cookie : cookies) {
+				if(cookie.getName().equals("value")) {
+					x = Integer.parseInt(cookie.getValue());
+					break;
+				}
+			}
+			
 			int y = value;
 			
 			// String op = (String) application.getAttribute("op");
-			String op = (String) session.getAttribute("op");
+			// String op = (String) session.getAttribute("op");
+			String op="";
+			for (Cookie cookie : cookies) {
+				if(cookie.getName().equals("op")) {
+					op = cookie.getValue();
+					break;
+				}
+			}			
+			
+			
 			int result=0;
 			
 			if(op.equals("+")) {
@@ -67,6 +100,9 @@ public class Calc2 extends HttpServlet{
 					return;
 				}
 			}
+			System.out.println("x:"+x);
+			System.out.println("y:"+y);
+			System.out.println("op:"+op);
 			
 			out.print(x+op+y+"="+result);			
 			
@@ -75,14 +111,33 @@ public class Calc2 extends HttpServlet{
 			// application 저장소에 변수값을 저장
 			// application.setAttribute("value", value);
 			// application.setAttribute("op", operator);	
-			session.setAttribute("value", value);
-			session.setAttribute("op", operator);	
+			// session.setAttribute("value", value);
+			// session.setAttribute("op", operator);	
+			
+			Cookie valueCookie =  new Cookie("value", String.valueOf(value));
+			Cookie opCookie =  new Cookie("op", operator);
+			
+			// path 설정
+			valueCookie.setPath("/calc_cookie");
+			opCookie.setPath("/calc_cookie");
+						
+			// maxAge 설정 - 24시간 유지
+			valueCookie.setMaxAge(60 * 60);
+			opCookie.setMaxAge(24 * 60 * 60);
+			
+			res.addCookie(valueCookie);
+			res.addCookie(opCookie);
 			
 			System.out.println("value : "+value);
 			System.out.println("operator : "+operator);
+			
+			// Redirection
+			res.sendRedirect("calc_cookie.html");
 		}	
 		
+		System.out.println("===> "+count);
 		
-	}
+		
+	}	
 
 }
