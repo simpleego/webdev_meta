20a43920ea9c286383df7bab378f73c9f96b56e4
Jong Cheon Park <simpleego@gmail.com>
2025-08-28 09:24:56 +0900

    Add files via upload

---
+++ BackEnd/Springframework/수업소스/스프링프레임워크/src/main/java/com/springbook/biz/BoardProjectApplication.java
@@ -0,0 +1,15 @@
+package com.springbook.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectApplication.class, args);
+        System.out.println("BoardProjectApplication started");
+
+    }
+
+}
